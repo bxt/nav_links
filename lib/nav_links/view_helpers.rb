b63cd30034e02c2a4db8bf14f5cf3f0917bc59f6
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-19 18:06:12 +0200

    Add new nav_links helper

--- lib/nav_links/view_helpers.rb
+++ lib/nav_links/view_helpers.rb
@@ -8,6 +8,27 @@ module NavLinks
 
       LinkGenerator.new(request, title, path, html_options, options).to_html
     end
+
+    def nav_links(options = {})
+      raise ArgumentError, "Missing block" unless block_given?
+      option_wrapper = NavLinksOptions.new(self, options)
+      yield option_wrapper
+    end
+
+    class NavLinksOptions
+      def initialize(base, options = {})
+        @base = base
+        @options = options
+      end
+
+      def nav_link_to(*args, &block)
+        index = block_given? ? 2 : 3
+        args[index] = @options.merge(args[index] || {})
+
+        @base.nav_link_to(*args, &block)
+      end
+
+    end
   end
 end
 
