0c458797c01526d532c50a451362025006bcb871
Bernhard Häussner <bxt@die-optimisten.net>
2013-09-19 15:27:18 +0200

    Add rspec and dummy testing rails app

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '029c4930027e877d48c1b8849e95d80edaba7b8880cfd25adc4bd2f5c3fdd592fe1b0790451dd1c737caf64fde4a2321e589b1a9161cbf338b11f98a178779b3'
